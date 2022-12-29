7934f131c65abffc9ce114f2660ad934d8fb1639
Laskellyb <anikwenzekelly@gmail.com>
2022-12-29 09:40:06 +0100

    installed chakra-ui kit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppProvider } from "./context";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </AppProvider>
   </React.StrictMode>
 );
