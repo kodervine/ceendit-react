2cbec8073cfcb43cdc3cd562247d8b3395fa8d76
Laskellyb <anikwenzekelly@gmail.com>
2022-12-29 10:16:38 +0100

    styled the date input and invoice form components

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useGlobalContext } from "../context";
+import { Input } from "@chakra-ui/react";
 
 // Deconstructing data gotten from the Invoice App and context.jsx. Date on the invoice page
 const DateInput = ({
@@ -12,8 +13,10 @@ const DateInput = ({
   return (
     <div>
       <p>{text}</p>
-      <input
-        type="date"
+      <Input
+        placeholder="Select Date and Time"
+        size="md"
+        type="datetime-local"
         name={nameOfDateInput}
         id="date-input"
         onChange={onHandleChange}
--- src/components/InvoiceForm.jsx
+++ src/components/InvoiceForm.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  Input,
+  Stack,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
 
 // Props for the input are gotten from InvoiceApp page, with data from the iseState declared on the useContext component
 const InvoiceForm = ({
@@ -16,34 +24,39 @@ const InvoiceForm = ({
     <section>
       <h3>{recipient}</h3>
       <div>
-        <input
-          type="email"
-          name={formNameOfInvoiceFormEmail}
-          id="invoice-form-email"
-          placeholder="Email"
-          value={valueOfInvoiceFormEmail}
-          onChange={onHandleChange}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          name={formNameOfInvoiceFormText}
-          id="invoice-form-name-text"
-          placeholder={placeholder}
-          value={valueOfInvoiceFormText}
-          onChange={onHandleChange}
-        />
-      </div>
-      <div>
-        <input
-          type="number"
-          name={formNameOfInvoiceFormNumber}
-          id="invoice-form-phonenumber"
-          placeholder="Phone number"
-          value={valueOfInvoiceFormPhoneNumber}
-          onChange={onHandleChange}
-        />
+        <Stack spacing={3}>
+          <Input
+            type="text"
+            variant="filled"
+            name={formNameOfInvoiceFormText}
+            id="invoice-form-name-text"
+            placeholder={placeholder}
+            value={valueOfInvoiceFormText}
+            onChange={onHandleChange}
+          />
+
+          <Input
+            type="number"
+            name={formNameOfInvoiceFormNumber}
+            id="invoice-form-phonenumber"
+            placeholder="Phone number"
+            variant="filled"
+            value={valueOfInvoiceFormPhoneNumber}
+            onChange={onHandleChange}
+          />
+
+          <Input
+            size="md"
+            variant="filled"
+            placeholder="Email"
+            type="email"
+            name={formNameOfInvoiceFormEmail}
+            id="invoice-form-email"
+            value={valueOfInvoiceFormEmail}
+            onChange={onHandleChange}
+            required
+          />
+        </Stack>
       </div>
     </section>
   );
