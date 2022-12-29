40955064192a49f24533f28661a2710d4606698e
Laskellyb <anikwenzekelly@gmail.com>
2022-12-29 08:46:55 +0100

    installed react router dom and set up form-preview path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InvoiceApp from "./pages/InvoiceApp";
+import FormPreview from "./pages/FormPreview";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <InvoiceApp />
-    </div>
+      <Routes>
+        {/* <Route exact path="/" component={InvoiceApp} /> */}
+        <Route path="/form-preview" component={FormPreview} />
+      </Routes>
+    </Router>
+    // <div className="App">
+    //
+    // </div>
   );
 }
 
