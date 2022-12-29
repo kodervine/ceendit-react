80b50f298829144269d56b23991304bff830b90e
Laskellyb <anikwenzekelly@gmail.com>
2022-12-29 10:31:02 +0100

    implemented chakra on the invoiceitems component

--- src/components/InvoiceItems.jsx
+++ src/components/InvoiceItems.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Stack, Input, Textarea, Heading } from "@chakra-ui/react";
 
 // Props for the input values are gotten from InvoiceApp page rendered with InvoiceItems name, with data from the useState declared on the useContext component
 const InvoiceItems = ({
@@ -14,43 +15,53 @@ const InvoiceItems = ({
 }) => {
   return (
     <div>
-      <h3>Item</h3>
       {/* Item name */}
-      <input
-        type="text"
-        name={nameOfInvoiceItemName}
-        id="item-name"
-        placeholder="Item name"
-        value={valueOfInvoiceItemName}
-        onChange={onHandleChange}
-      />
-      {/* Item Contentt */}
-      <input
-        type="text"
-        name={nameOfInvoiceItemContent}
-        id="item-content"
-        placeholder="Content owed to you"
-        value={valueOfInvoiceItemContent}
-        onChange={onHandleChange}
-      />
-      {/* qty */}
-      <h4>Qty</h4>
-      <input
-        type="number"
-        name={nameOfInvoiceItemQty}
-        id="item-qty"
-        value={valueOfInvoiceItemQty}
-        onChange={onHandleChange}
-      />
-      {/* Total */}
-      <h4>Total</h4>
-      <input
-        type="text"
-        name={nameOfInvoiceItemTotal}
-        id="item-total"
-        value={valueOfInvoiceItemTotal}
-        onChange={onHandleChange}
-      />
+      <Stack spacing={3}>
+        <Heading as="h4" size="md">
+          Item
+        </Heading>
+        <Input
+          type="text"
+          name={nameOfInvoiceItemName}
+          id="item-name"
+          placeholder="Item name"
+          value={valueOfInvoiceItemName}
+          onChange={onHandleChange}
+        />
+        {/* Item Contentt */}
+        <Textarea
+          size="sm"
+          name={nameOfInvoiceItemContent}
+          type="text"
+          id="item-content"
+          placeholder="Content owed to you"
+          value={valueOfInvoiceItemContent}
+          onChange={onHandleChange}
+        />
+
+        {/* qty */}
+        <Heading as="h4" size="md">
+          Qty
+        </Heading>
+        <Input
+          type="number"
+          name={nameOfInvoiceItemQty}
+          id="item-qty"
+          value={valueOfInvoiceItemQty}
+          onChange={onHandleChange}
+        />
+        {/* Total */}
+        <Heading as="h4" size="md">
+          Total
+        </Heading>
+        <Input
+          type="number"
+          name={nameOfInvoiceItemTotal}
+          id="item-total"
+          value={valueOfInvoiceItemTotal}
+          onChange={onHandleChange}
+        />
+      </Stack>
     </div>
   );
 };
