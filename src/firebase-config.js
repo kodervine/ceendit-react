48f2ab17a851e2880d2bf3bbff17f0b6a1a26d4a
Laskellyb <anikwenzekelly@gmail.com>
2023-01-17 09:16:31 +0100

    configuring the firebase app

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_b1GrTINF64uBHvXxW1LZhN1NLRrGnkw",
+  authDomain: "ceendit.firebaseapp.com",
+  projectId: "ceendit",
+  storageBucket: "ceendit.appspot.com",
+  messagingSenderId: "690103606444",
+  appId: "1:690103606444:web:3082b5b7f46e5b8a2c5309",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
