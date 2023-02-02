0e21d0c524466c2ef676be6528588d29b768e275
Laskellyb <anikwenzekelly@gmail.com>
2023-02-02 13:55:27 +0100

    Added toggle light and dark mode on the signin pages

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -16,15 +16,17 @@ import {
   FormControl,
   FormHelperText,
   InputRightElement,
+  IconButton,
   useColorMode,
 } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { FaUserAlt, FaLock } from "react-icons/fa";
 
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
 
 const SignInPage = () => {
-  const { colorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   // Handle form and showing of password
   const [showPassword, setShowPassword] = useState(false);
   const handleShowClick = () => setShowPassword(!showPassword);
@@ -62,89 +64,101 @@ const SignInPage = () => {
   };
 
   return (
-    <Flex
-      flexDirection="column"
-      width="100wh"
-      height="100vh"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Stack
-        flexDir="column"
-        mb="2"
+    <>
+      <IconButton
+        mr="10"
+        w={6}
+        h={6}
+        p={5}
+        textAlign="end"
+        onClick={toggleColorMode}
+      >
+        {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+      </IconButton>
+      <Flex
+        flexDirection="column"
+        width="100wh"
+        height="100vh"
         justifyContent="center"
         alignItems="center"
-        width={{ base: "90vw", md: "468px" }}
-        padding="50px 10px 30px 10px"
-        boxShadow={colorMode === "light" ? "2xl" : "dark-lg"}
-        borderRadius="20px"
       >
-        <Avatar bg="blue.500" />
-        <Heading color="blue.400">Welcome back</Heading>
-        <Text>Pick Up where you Left Off</Text>
-        <Box minW={{ base: "100%", md: "468px" }}>
-          <form onSubmit={handleLogInFormSubmit}>
-            <Stack spacing={5} p="1rem" boxShadow="md">
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<CFaUserAlt color="gray.300" />}
-                  />
-                  <Input
-                    type="email"
-                    placeholder="email address"
-                    name="signupEmail"
-                    value={userSignUpForm.email}
-                    onChange={handleLogInFormChange}
-                  />
-                </InputGroup>
-              </FormControl>
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    color="gray.300"
-                    children={<CFaLock color="gray.300" />}
-                  />
-                  <Input
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Password"
-                    name="signupPassword"
-                    value={userSignUpForm.password}
-                    onChange={handleLogInFormChange}
-                  />
-                  <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                      {showPassword ? "Hide" : "Show"}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <FormHelperText textAlign="right">
-                  <Link>forgot password?</Link>
-                </FormHelperText>
-              </FormControl>
-              <Button
-                borderRadius="50px"
-                type="submit"
-                variant="solid"
-                colorScheme="blue"
-                width="full"
-              >
-                Log in
-              </Button>
-            </Stack>
-          </form>
-        </Box>
-        <Box>
-          New to us?{" "}
-          <Link to="/create-account">
-            {" "}
-            <span style={{ color: "blue" }}>Sign Up here</span>
-          </Link>
-        </Box>
-      </Stack>
-    </Flex>
+        <Stack
+          flexDir="column"
+          mb="2"
+          justifyContent="center"
+          alignItems="center"
+          width={{ base: "90vw", md: "468px" }}
+          padding="50px 10px 30px 10px"
+          boxShadow={colorMode === "light" ? "2xl" : "dark-lg"}
+          borderRadius="20px"
+        >
+          <Avatar bg="blue.500" />
+          <Heading color="blue.400">Welcome back</Heading>
+          <Text>Pick Up where you Left Off</Text>
+          <Box minW={{ base: "100%", md: "468px" }}>
+            <form onSubmit={handleLogInFormSubmit}>
+              <Stack spacing={5} p="1rem" boxShadow="md">
+                <FormControl>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<CFaUserAlt color="gray.300" />}
+                    />
+                    <Input
+                      type="email"
+                      placeholder="email address"
+                      name="signupEmail"
+                      value={userSignUpForm.email}
+                      onChange={handleLogInFormChange}
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      color="gray.300"
+                      children={<CFaLock color="gray.300" />}
+                    />
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      name="signupPassword"
+                      value={userSignUpForm.password}
+                      onChange={handleLogInFormChange}
+                    />
+                    <InputRightElement width="4.5rem">
+                      <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                        {showPassword ? "Hide" : "Show"}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                  <FormHelperText textAlign="right">
+                    <Link>forgot password?</Link>
+                  </FormHelperText>
+                </FormControl>
+                <Button
+                  borderRadius="50px"
+                  type="submit"
+                  variant="solid"
+                  colorScheme="blue"
+                  width="full"
+                >
+                  Log in
+                </Button>
+              </Stack>
+            </form>
+          </Box>
+          <Box>
+            New to us?{" "}
+            <Link to="/create-account">
+              {" "}
+              <span style={{ color: "blue" }}>Sign Up here</span>
+            </Link>
+          </Box>
+        </Stack>
+      </Flex>
+    </>
   );
 };
 
