1ca04daa2b290215683c663695ca80a31017baa8
Laskellyb <anikwenzekelly@gmail.com>
2023-02-03 11:16:50 +0100

    tryna understand testing in javascript

--- src/__tests__/signinpage.test.js
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import SignInPage from "../pages/SignInPage";
-
-describe("SignInPage test", () => {
-  // test goes here
-});
