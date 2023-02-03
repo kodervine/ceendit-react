82b7cf938dbdb5aac91aed1bb8d272ff07de33fe
Laskellyb <anikwenzekelly@gmail.com>
2023-02-03 11:12:17 +0100

    installed vitest for testing

---
+++ src/__tests__/signinpage.test.js
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import SignInPage from "../pages/SignInPage";
+
+describe("SignInPage test", () => {
+  // test goes here
+});
