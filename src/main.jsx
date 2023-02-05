f3519c0ce5df64c0e99fd970202ae2150dd266c3
Laskellyb <anikwenzekelly@gmail.com>
2023-02-05 09:15:57 +0100

    moved the context to a specific file AppContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AppProvider } from "./context";
+import { AppProvider } from "./context/AppContext";
+import { UserProvider } from "./context/UserContext";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { UserProvider } from "./context/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
