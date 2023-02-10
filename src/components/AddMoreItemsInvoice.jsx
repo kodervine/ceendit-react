ae13e5cef29d6a464a3c75a446abf328b25d9ed1
Laskellyb <anikwenzekelly@gmail.com>
2023-02-10 09:31:33 +0100

    made the qty and price to be flex items

--- src/components/AddMoreItemsInvoice.jsx
+++ src/components/AddMoreItemsInvoice.jsx
@@ -1,38 +1,56 @@
-import React from "react";
-import { Stack, Input, Textarea, Heading } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Stack, Input, Textarea, Heading, Box, Flex } from "@chakra-ui/react";
 
 const AddMoreItemsInvoice = () => {
+  const [addedInvoiceContent, setAddedInvoiceContent] = useState("");
+  const [addedInvoiceQty, setAddedInvoiceQty] = useState("");
+  const [addedInvoicePrice, setAddedInvoicePrice] = useState("");
+
   return (
     <Stack spacing={3}>
-      <Heading as="h4" size="md">
+      <Heading as="h4" size="sm">
         Item
       </Heading>
       {/* Item Content */}
       <Textarea
         size="sm"
-        name="Item content"
+        name="addedInvoiceContent"
         type="text"
-        id="item-content"
-        placeholder="Content owed to you"
-        value=""
+        placeholder="Work you did"
+        value={addedInvoiceContent}
+        onChange={(e) => setAddedInvoiceContent(e.target.value)}
       />
 
       {/* qty */}
-      <Heading as="h4" size="md">
-        Qty
-      </Heading>
-      <Input type="number" name="qty" id="item-qty" value="" />
-
-      <Heading as="h4" size="md">
-        Price
-      </Heading>
-      <Input
-        type="number"
-        name="Price of item"
-        id="price"
-        placeholder="Item Price"
-        value=""
-      />
+      <Flex
+        alignItems="cen
+      "
+        gap="2"
+      >
+        <Box w="100%">
+          <Heading as="h4" size="sm" pb="2">
+            Qty
+          </Heading>
+          <Input
+            type="number"
+            name="addedInvoiceQty"
+            value={addedInvoiceQty}
+            onChange={(e) => setAddedInvoiceQty(e.target.value)}
+          />
+        </Box>
+        <Box w="100%">
+          <Heading as="h4" size="sm" pb="2">
+            Price
+          </Heading>
+          <Input
+            type="number"
+            name="addedInvoicePrice"
+            placeholder="Item Price"
+            value={addedInvoicePrice}
+            onChange={(e) => setAddedInvoicePrice(e.target.value)}
+          />
+        </Box>
+      </Flex>
     </Stack>
   );
 };
--- src/components/InvoiceItems.jsx
+++ src/components/InvoiceItems.jsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Stack, Input, Textarea, Heading, Button } from "@chakra-ui/react";
+import {
+  Stack,
+  Input,
+  Textarea,
+  Heading,
+  Flex,
+  Box,
+  Button,
+} from "@chakra-ui/react";
 import AddMoreItemsInvoice from "./AddMoreItemsInvoice";
 
 // Props for the input values are gotten from InvoiceApp page rendered with InvoiceItems name, with data from the useState declared on the useContext component
@@ -36,31 +44,34 @@ const InvoiceItems = ({
           value={valueOfInvoiceItemContent}
           onChange={onHandleChange}
         />
-
-        {/* qty */}
-        <Heading as="h4" size="md">
-          Qty
-        </Heading>
-        <Input
-          type="number"
-          name={nameOfInvoiceItemQty}
-          id="item-qty"
-          value={valueOfInvoiceItemQty}
-          onChange={onHandleChange}
-        />
-
-        <Heading as="h4" size="md">
-          Price
-        </Heading>
-        <Input
-          type="number"
-          name={nameOfInvoiceItemPrice}
-          id="item-price"
-          placeholder="Item Price"
-          value={valueOfInvoiceItemPrice}
-          onChange={onHandleChange}
-        />
-
+        <Flex gap="2" alignItems="center">
+          {/* qty */}
+          <Box>
+            <Heading as="h4" size="sm" pb="2">
+              Qty
+            </Heading>
+            <Input
+              type="number"
+              name={nameOfInvoiceItemQty}
+              id="item-qty"
+              value={valueOfInvoiceItemQty}
+              onChange={onHandleChange}
+            />
+          </Box>
+          <Box>
+            <Heading as="h4" size="sm" pb="2">
+              Price
+            </Heading>
+            <Input
+              type="number"
+              name={nameOfInvoiceItemPrice}
+              id="item-price"
+              placeholder="Item Price"
+              value={valueOfInvoiceItemPrice}
+              onChange={onHandleChange}
+            />{" "}
+          </Box>
+        </Flex>
         {addedInvoiceItemsArray.map((items, i) => {
           return <AddMoreItemsInvoice text={items} />;
         })}
