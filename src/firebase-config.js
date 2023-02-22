95547b71a25fc425c511d919ec0dccb94e19667b
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-02-22 10:11:46 +0100

    build(firebase-config): moves logout function to `src/utils/auth`

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -28,9 +28,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const logOutUser = () => {
-  signOut(auth);
-};
-
 export default app;
-export { auth, db, logOutUser };
+export { auth, db };
