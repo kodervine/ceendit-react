675ec906e4fe355f2c52a48d5ca07d16daf3eaee
Laskellyb <anikwenzekelly@gmail.com>
2023-03-08 11:27:16 +0100

    chore: building the color scheme

--- src/components/settings/SetNightMode.jsx
+++ src/components/settings/SetNightMode.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { Box, useColorMode, Flex, Text } from "@chakra-ui/react";
 import { BsToggle2Off, BsToggle2On } from "react-icons/bs";
 import { useThemeContext } from "@/context/ThemeContext";
 
 const SetNightMode = () => {
   const { isEvening } = useThemeContext();
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <div>
-      SetNightMode
-      {isEvening ? <BsToggle2Off /> : <BsToggle2On />}
+      <Text>Color Scheme</Text>
+      <Flex justifyContent="space-between">
+        <Box height={20} backgroundColor="blue">
+          <Text>Light mode</Text>
+        </Box>
+        <Box height={20} backgroundColor="gray">
+          <Text>Dark mode</Text>
+        </Box>
+      </Flex>
+      <Flex justifyContent="space-between">
+        {" "}
+        <Text>Schedule Night Mode</Text>
+        {isEvening ? <BsToggle2Off /> : <BsToggle2On />}
+      </Flex>
+
+      <Text>Turn on</Text>
+      <Text>Turn off</Text>
     </div>
   );
 };
